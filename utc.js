9d9d9a54e7bf408c37e4124d8244590fb30b09ee
Divyanshu Saini <Divyanshu-Saini@users.noreply.github.com>
2017-10-27 14:29:40 +0530

    Unis timestamp converted to local time stamp

---
+++ utc.js
@@ -0,0 +1,20 @@
+
+module.exports = function(unix_timestamp){
+    if(typeof unix_timestamp === 'number' || isNaN(unix_timestamp)){
+        return -1;
+    }
+    var date = new Date(unix_timestamp*1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    
+    return(formattedTime);
+}
+
+
+
